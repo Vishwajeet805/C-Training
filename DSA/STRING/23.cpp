65c05a23ecfef38c1731923ea09f006b54cb6dc1
Vishwajeet805 <vishwajeetsingh80558055@gmail.com>
2025-10-23 15:40:18 +0530

    35 DSA Questions Added

--- DSA/STRING/23.cpp
+++ DSA/STRING/23.cpp
@@ -1,6 +1,20 @@
-#include <stdio.H>
+#include <stdio.h>
+#include <string>
+#include <iostream>
+using namespace std;
 int main(){
-    printf ("Hello world");
+    string str="dad";
+    int n=str.length();
+    string result="";
+    for (int i=0;i<n;i++){
+        int j;
+        for (j=0;j<result.length();j++){
+            if(str[i]==result[j])
+                break;
+        }
+        if(j==result.length())
+         result +=str[i];
+    }
+    cout <<"Updated string :"<<result;
     return 0;
-    
 }
--- DSA/STRING/24.cpp
+++ DSA/STRING/24.cpp
@@ -1,6 +1,47 @@
-#include <stdio.H>
-int main(){
-    printf ("Hello world");
-    return 0;
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s;
+    cout << "Enter string: ";
+    cin >> s;
+
+    int n = s.length();
+
     
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (s[j] > s[j + 1]) {
+                char temp = s[j];
+                s[j] = s[j + 1];
+                s[j + 1] = temp;
+            }
+        }
+    }
+
+   
+    int stack[10] = {0}; 
+    cout << s << "\n";   
+    int i = 0;
+    while (i < n) {
+        if (stack[i] < i) {
+            if (i % 2 == 0) {
+                char temp = s[0];
+                s[0] = s[i];
+                s[i] = temp;
+            } else {
+                char temp = s[stack[i]];
+                s[stack[i]] = s[i];
+                s[i] = temp;
+            }
+            cout << s << "\n";
+            stack[i]++;
+            i = 0;
+        } else {
+            stack[i] = 0;
+            i++;
+        }
+    }
+
+    return 0;
 }
