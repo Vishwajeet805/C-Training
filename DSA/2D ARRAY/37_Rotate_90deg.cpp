6f574e478a4da594152a20392c3df68682cc4cea
Vishwajeet805 <vishwajeetsingh80558055@gmail.com>
2025-10-27 23:39:11 +0530

    50 DSA Questions Completed

---
+++ DSA/2D ARRAY/37_Rotate_90deg.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+    int n=3;
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+            swap(a[i][j],a[j][i]);
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n/2;j++){
+            swap(a[i][j],a[i][n-j-1]);
+        }
+    }
+    cout<<"Matrix after 90 degree rotation:"<<endl;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<a[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
