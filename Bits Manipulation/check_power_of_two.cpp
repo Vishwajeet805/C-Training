6b2deb994b61cc29fcd5bfcf7f96c528b1346184
Vishwajeet805 <vishwajeetsingh80558055@gmail.com>
2025-10-28 23:37:58 +0530

    Bits Manipulation

---
+++ Bits Manipulation/check_power_of_two.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n = 16;
+    if (n && !(n & (n - 1)))
+        cout << n << " is a power of 2";
+    else
+        cout << n << " is not a power of 2";
+    return 0;
+}
