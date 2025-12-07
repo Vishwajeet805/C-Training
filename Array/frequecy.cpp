944d1212d5a30baf428c02392e763f9da48f39a1
Vishwajeet805 <vishwajeetsingh80558055@gmail.com>
2025-12-07 23:07:52 +0530

    Cpp Practice

--- Array/frequecy.cpp
+++ Array/frequecy.cpp
@@ -9,6 +9,16 @@ int main()
             max = arr[i];
         }
     }
-
+    int ar[10]={1,2,2,3,4,4,4,5,5,5};
+    int freq[10] = {0};
+    for (int i = 0; i < 10; i++){
+        freq[ar[i]]++;  
+    }
+    printf("Maximum value in the array is: %d\n", max);
+    printf("Frequency of elements in the array:\n");
+    for (int i = 0; i < 10; i++){
+        if (freq[i] != 0){
+            printf("Element %d: %d times\n", i, freq[i]);   
+         }} 
 
 }
