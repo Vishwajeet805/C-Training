f8d4336b0450b8a8f7c0cddf2f23ded5b22576fa
Vishwajeet805 <vishwajeetsingh80558055@gmail.com>
2025-12-13 22:01:29 +0530

    Practice Questions Added

--- Array/output/Newtest2.cpp
+++ Array/output/Newtest2.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 using namespace std;
-
+//5. Longest Word in a String
 int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
+    char s[100];
+    cin.getline(s,100);
+
+    int maxLen=0, curr=0;
+    for(int i=0;s[i]!='\0';i++){
+        if(s[i]!=' ')
+            curr++;
+        else{
+            if(curr>maxLen) maxLen=curr;
+            curr=0;
+        }
+    }
+    if(curr>maxLen) maxLen=curr;
+    cout<<maxLen;
 }
--- Array/output/Newtest3.cpp
+++ Array/output/Newtest3.cpp
@@ -1,7 +1,29 @@
+//5. Longest Word in a String
 #include <iostream>
 using namespace std;
 
-int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
+#define MAX 5
+int stack[MAX], top=-1;
+
+void push(int x){
+    if(top==MAX-1){
+        cout<<"Overflow\n";
+        return;
+    }
+    stack[++top]=x;
+}
+
+void pop(){
+    if(top==-1){
+        cout<<"Underflow\n";
+        return;
+    }
+    cout<<"Popped "<<stack[top--]<<endl;
+}
+
+int main(){
+    push(10);
+    push(20);
+    pop();
+    pop();
 }
