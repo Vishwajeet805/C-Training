e247940db4831aae7b6032bbabfe8ae7602e0a88
Vishwajeet805 <vishwajeetsingh80558055@gmail.com>
2025-12-13 22:00:15 +0530

    Practice Questions Added

--- Array/output/newtest.cpp
+++ Array/output/newtest.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <map>
 using namespace std;
-
+//3. Count Frequency Using Map
 int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
+    int n;
+    cin>>n;
+    int a[n];
+    map<int,int> freq;
+
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+        freq[a[i]]++;
+    }
+
+    for(auto i:freq)
+        cout<<i.first<<" -> "<<i.second<<endl;
 }
--- Array/output/newtest1.cpp
+++ Array/output/newtest1.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 using namespace std;
-
+//4. Remove Duplicates from Array
 int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
+    int n;
+    cin>>n;
+    int a[n], temp[n], k=0;
+
+    for(int i=0;i<n;i++) cin>>a[i];
+
+    for(int i=0;i<n;i++){
+        int flag=0;
+        for(int j=0;j<k;j++){
+            if(a[i]==temp[j]){
+                flag=1;
+                break;
+            }
+        }
+        if(flag==0)
+            temp[k++]=a[i];
+    }
+
+    for(int i=0;i<k;i++) cout<<temp[i]<<" ";
 }
