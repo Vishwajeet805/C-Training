2d273297ceb97b26634a445ab44c982c81945521
Vishwajeet805 <vishwajeetsingh80558055@gmail.com>
2025-12-13 22:04:57 +0530

    Practice Questions Added

--- Array/output/newtest6.cpp
+++ Array/output/newtest6.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
-
+// C++ File Handling - Writing to a File
 int main() {
-    cout << "Hello, World!" << endl;
+    ofstream fout("data.txt");
+    fout << "C++ File Handling Example\n";
+    fout << "Writing data into file";
+    fout.close();
     return 0;
 }
--- Array/output/newtest7.cpp
+++ Array/output/newtest7.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
-
+// C++ File Handling - Reading from a File
 int main() {
-    cout << "Hello, World!" << endl;
+    ifstream fin("data.txt");
+    char ch;
+    while(fin.get(ch)) {
+        cout << ch;
+    }
+    fin.close();
     return 0;
 }
