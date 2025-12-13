7228c24185f4b7907009570c74af079119ea3f6b
Vishwajeet805 <vishwajeetsingh80558055@gmail.com>
2025-12-13 22:02:36 +0530

    Practice Questions Added

--- Array/output/newtest5.cpp
+++ Array/output/newtest5.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include <stack>
 using namespace std;
+// Reverse a String using Stack
+int main(){
+    string s;
+    cin>>s;
+    stack<char> st;
 
-int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
+    for(char c:s) st.push(c);
+    while(!st.empty()){
+        cout<<st.top();
+        st.pop();
+    }
 }
