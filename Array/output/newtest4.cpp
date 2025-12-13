62d7bb06817cb21bd15e23f0e9ad6995c4907b49
Vishwajeet805 <vishwajeetsingh80558055@gmail.com>
2025-12-13 22:02:02 +0530

    Practice Questions Added

--- Array/output/newtest4.cpp
+++ Array/output/newtest4.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 using namespace std;
+// Queue Implementation
+#define MAX 5
+int q[MAX], front=-1, rear=-1;
 
-int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
+void enqueue(int x){
+    if(rear==MAX-1){
+        cout<<"Queue Full\n";
+        return;
+    }
+    if(front==-1) front=0;
+    q[++rear]=x;
+}
+
+void dequeue(){
+    if(front>rear || front==-1){
+        cout<<"Queue Empty\n";
+        return;
+    }
+    cout<<"Removed "<<q[front++]<<endl;
+}
+
+int main(){
+    enqueue(5);
+    enqueue(15);
+    dequeue();
 }
