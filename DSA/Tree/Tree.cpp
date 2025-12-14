bd43dbf1471082f71ab8dc51ca4b5561cb1e4c95
Vishwajeet805 <vishwajeetsingh80558055@gmail.com>
2025-12-14 21:27:02 +0530

    Added Tree to DSA Section

---
+++ DSA/Tree/Tree.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+Node* createNode(int value) {
+    Node* newNode = new Node();
+    newNode->data = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+void inorder(Node* root) {
+    if (root == NULL) return;
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+void postorder(Node* root) {
+    if (root == NULL) return;
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
+
+void preorder(Node* root) {
+    if (root == NULL) return;
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+int main() {
+    Node* root = createNode(10);
+    root->left = createNode(5);
+    root->right = createNode(15);
+
+    cout << "Inorder Traversal: ";
+    inorder(root);
+    cout << "\nPreorder Traversal: ";
+    preorder(root);
+    cout << "\nPostorder Traversal: ";
+    postorder(root);
+    return 0;
+}
