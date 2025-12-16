c42a46b9e8c61beb35511f88f4ec50c6e5d9c932
Vishwajeet805 <vishwajeetsingh80558055@gmail.com>
2025-12-16 23:13:03 +0530

    Added Recursion Practice Questions

---
+++ Recursion/Tower_Of_Hanoi.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+void toh(int n, char s, char a, char d)
+{
+    if (n == 0)
+        return;
+    toh(n - 1, s, d, a);
+    cout << s << " -> " << d << endl;
+    toh(n - 1, a, s, d);
+}
+int main() { 
+    toh(3, 'A', 'B', 'C'); 
+}
