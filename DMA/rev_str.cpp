716dea0b372a941e93feafb39072a7751cc0466a
Vishwajeet805 <vishwajeetsingh80558055@gmail.com>
2025-12-20 23:25:47 +0530

    Added DMA Questions

---
+++ DMA/rev_str.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    int n,i;
+    scanf("%d",&n);
+    int *a = (int *)malloc(n*sizeof(int));
+    for(i=0;i<n;i++) scanf("%d",&a[i]);
+    for(i=n-1;i>=0;i--) printf("%d ",a[i]);
+    free(a);
+}
