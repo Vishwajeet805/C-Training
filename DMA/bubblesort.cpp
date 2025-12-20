2f3d61b3e1e4e0c8f7a1b1c407ac9ac802878357
Vishwajeet805 <vishwajeetsingh80558055@gmail.com>
2025-12-20 23:28:35 +0530

    Added DMA Questions

---
+++ DMA/bubblesort.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    int n,i,j,t;
+    scanf("%d",&n);
+    int *a = (int *)malloc(n*sizeof(int));
+    for(i=0;i<n;i++) scanf("%d",&a[i]);
+    for(i=0;i<n-1;i++)
+      for(j=0;j<n-1-i;j++)
+        if(a[j]>a[j+1]){ t=a[j]; a[j]=a[j+1]; a[j+1]=t; }
+    for(i=0;i<n;i++) printf("%d ",a[i]);
+    free(a);
+}
