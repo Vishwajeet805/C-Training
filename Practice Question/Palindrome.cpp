160732e562008b308a0cf2b1d94f982eb9df5895
Vishwajeet805 <vishwajeetsingh80558055@gmail.com>
2025-12-25 02:22:09 +0530

    Added Practice Questions

---
+++ Practice Question/Palindrome.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 121, rev = 0, temp = n;
+
+    while (temp > 0) {
+        rev = rev * 10 + temp % 10;
+        temp /= 10;
+    }
+
+    if (rev == n)
+        cout << "Palindrome";
+    else
+        cout << "Not Palindrome";
+}
