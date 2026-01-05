558e4a5b156a6f86c4b036b16e7a8376f4197ae7
Vishwajeet805 <vishwajeetsingh80558055@gmail.com>
2026-01-05 23:49:46 +0530

    Added Practice Question

---
+++ Practice Question/Dutch_National_Flag.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int nums[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &nums[i]);
+    }
+
+    int low = 0, mid = 0, high = n - 1;
+    int temp;
+
+    while (mid <= high) {
+        if (nums[mid] == 0) {
+            temp = nums[low];
+            nums[low] = nums[mid];
+            nums[mid] = temp;
+            low++;
+            mid++;
+        } else if (nums[mid] == 1) {
+            mid++;
+        } else { 
+            temp = nums[mid];
+            nums[mid] = nums[high];
+            nums[high] = temp;
+            high--;
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", nums[i]);
+    }
+
+    return 0;
+}
