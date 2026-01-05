2b40a6641f109165ef56fd709eeeda4bfd0d16f2
Vishwajeet805 <vishwajeetsingh80558055@gmail.com>
2026-01-05 23:56:53 +0530

    Added Practice Question

--- Practice Question/Maxsum.cpp
+++ Practice Question/Maxsum.cpp
@@ -3,13 +3,6 @@ using namespace std;
 int main() {
 
     int n;
-    cin >> n;
-
-    int arr[n];
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
-int n;
     scanf("%d", &n);
 
     int arr[n];
